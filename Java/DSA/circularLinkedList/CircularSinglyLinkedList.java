780411bf28be45e6ce0b515d2429663add76b6ca
asf0 <scorpionspfc@gmail.com>
2025-04-15 10:23:29 -0600

    studied delete and search

--- Java/DSA/circularLinkedList/CircularSinglyLinkedList.java
+++ Java/DSA/circularLinkedList/CircularSinglyLinkedList.java
@@ -53,10 +53,77 @@ public class CircularSinglyLinkedList {
             for(int i = 0; i < size; i++) {
                 System.out.print(temp.value + " ");
                 if (i != size - 1) {
-                    System.out.print("->");
+                    System.out.print("-> ");
                 }
                 temp = temp.next;
             }
+            System.out.println("");
+        }
+    }
+
+    public boolean searchNode(int nodeValue) {
+        if (head == null) {
+            System.out.println("Linked list is empty");
+            return false;
+        } else {
+            Node temp = head;
+            for (int i = 0; i < size; i++) {
+                if (temp.value == nodeValue) {
+                    System.out.println("Node found at location: " + i);
+                    return true;
+                }
+                temp = temp.next;
+            }
+            System.out.println("Node not found");
+            return false;
+        }
+    }
+    public void deleteNode(int location) {
+        if (head == null) {
+            System.out.println("The CSLL does not exist");
+            return;
+        }
+        else if (location == 0) {
+            head = head.next;
+            tail.next = head;
+            size --;
+            if (size == 0) {
+                tail = null;
+                head.next = null;
+                head = null;
+            }
+        } else if (location >= size) {
+            Node temp = head;
+            for (int i = 0; i < size -1; i++) {
+                temp = temp.next;
+            }
+            if (temp == head) {
+                tail = null;
+                head.next = null;
+                head = null;
+                size = 0;
+                return;
+            }
+            temp.next = head;
+            tail = temp;
+            size--;
+        }   else {
+            Node temp = head;
+            for (int i = 0; i < location - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            size--;
+        }
+    }
+    public void deleteCSLL() {
+        if (head == null) {
+            System.out.println("The CSLL does not exist");
+        } else {
+            head = null;
+            tail.next = null;
+            tail = null;
+            System.out.println("The CSLL has been deleted");
         }
     }
 }
--- Java/DSA/circularLinkedList/Main.java
+++ Java/DSA/circularLinkedList/Main.java
@@ -6,7 +6,14 @@ public class Main {
         csll.createCSLL(5);
         csll.inserCSLL(4,0);
         csll.inserCSLL(3,8);
-        System.out.println("CSLL created with value: " + csll.head.value);
+        csll.inserCSLL(2,9);
         csll.traverseCSLL();
+        System.out.println("CSLL created with value: " + csll.head.value);
+        csll.deleteNode(0);
+        csll.traverseCSLL();
+        csll.deleteNode(1);
+        csll.traverseCSLL();
+        csll.deleteCSLL();
+
     }
 }
